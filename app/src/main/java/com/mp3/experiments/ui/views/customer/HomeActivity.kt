97a8e56525825e4647aa7ea40f694585379e5a16
NathanL014 <138946052+NathanL014@users.noreply.github.com>
2023-11-13 12:30:20 +0800

    HomeActivity updated

--- app/src/main/java/com/mp3/experiments/ui/views/customer/HomeActivity.kt
+++ app/src/main/java/com/mp3/experiments/ui/views/customer/HomeActivity.kt
@@ -2,11 +2,62 @@ package com.mp3.experiments.ui.views.customer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ActionTypes
+import com.denzcoskun.imageslider.constants.AnimationTypes
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemChangeListener
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.interfaces.TouchListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.mp3.experiments.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider) // init imageSlider
+
+        val imageList = ArrayList<SlideModel>() // Create image list
+        imageList.add(SlideModel("https://www.cnet.com/a/img/resize/e40db3b1ecae8c7de688981520ac2c3d8c7c9617/hub/2023/08/16/23d96691-7b64-4113-81f8-71f8430f9aff/fast-x-peacock.jpg?auto=webp&width=1200"))
+        imageList.add(SlideModel("https://assets-prd.ignimgs.com/2023/10/17/spidermanacrossthespiderversereview-blogroll-1685535068711-1697563405199.jpg"))
+        imageList.add(SlideModel("https://www.barbie-themovie.com/images/share.jpg"))
+        imageList.add(SlideModel("https://images.everyeye.it/img-notizie/the-marvels-marvel-annuncia-sorpresa-sceneggiatore-copione-scritto-8-mani-v3-627413.jpg"))
+        imageList.add(SlideModel("https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/campaign/fnaf/fnaf-trailer-thumbnail.jpg/_jcr_content/renditions/original.JPG"))
+
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+
+        imageSlider.setSlideAnimation(AnimationTypes.ZOOM_OUT)
+
+        imageSlider.setItemClickListener(object : ItemClickListener {
+            override fun onItemSelected(position: Int) {
+                // You can listen here.
+                println("normal")
+            }
+
+            override fun doubleClick(position: Int) {
+                // Do not use onItemSelected if you are using a double click listener at the same time.
+                // Its just added for specific cases.
+                // Listen for clicks under 250 milliseconds.
+                println("its double")
+            }
+        })
+
+        imageSlider.setItemChangeListener(object : ItemChangeListener {
+            override fun onItemChanged(position: Int) {
+                //println("Pos: " + position)
+            }
+        })
+
+        imageSlider.setTouchListener(object : TouchListener {
+            override fun onTouched(touched: ActionTypes, position: Int) {
+                if (touched == ActionTypes.DOWN){
+                    imageSlider.stopSliding()
+                } else if (touched == ActionTypes.UP ) {
+                    imageSlider.startSliding(5000)
+                }
+            }
+        })
     }
 }
